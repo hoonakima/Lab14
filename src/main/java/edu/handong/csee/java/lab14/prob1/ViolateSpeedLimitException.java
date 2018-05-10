7c06010e8dc80c3ae3662ed07ab4fa414445fca5
JoJeonghoon <akima95@naver.com>
2018-05-10 20:34:10 +0900

    Add single line comments for ViolateSpeedLimitException class

--- src/main/java/edu/handong/csee/java/lab14/prob1/ViolateSpeedLimitException.java
+++ src/main/java/edu/handong/csee/java/lab14/prob1/ViolateSpeedLimitException.java
@@ -1,13 +1,13 @@
-package edu.handong.csee.java.lab14.prob1;
+package edu.handong.csee.java.lab14.prob1; //package name
 
-public class ViolateSpeedLimitException extends Exception {
+public class ViolateSpeedLimitException extends Exception { //define my own exception. It should be a derived class of Exception class.
 
-	public ViolateSpeedLimitException() {
-		super();
+	public ViolateSpeedLimitException() { //default constructor
+		super(); //call base class constructor
 	}
 	
-	public ViolateSpeedLimitException(String message) {
+	public ViolateSpeedLimitException(String message) { //constructor which parameter is String
 	
-		super(message);
+		super(message); // The message parameter will be used in getMessage() method.
 	}
 }
