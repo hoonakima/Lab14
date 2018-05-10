d6d2b0aa0f5b11075494ac7844ba2c0f5e4cd0ae
JoJeonghoon <akima95@naver.com>
2018-05-10 23:15:27 +0900

    Add single line comments for In_exception class

--- src/main/java/edu/handong/csee/java/lab14/prob2/In_exception.java
+++ src/main/java/edu/handong/csee/java/lab14/prob2/In_exception.java
@@ -1,40 +1,42 @@
-package edu.handong.csee.java.lab14.prob2;
+package edu.handong.csee.java.lab14.prob2; //package name
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.InputMismatchException; //import InputMismatchException class
+import java.util.Scanner; //import Scanner class
 
-public class In_exception{
+public class In_exception{ //define In_exception class
 
-	private int x, y = 0;
+	private int x, y = 0; //private instance variable
 	
-	public In_exception() {
+	public In_exception() { //constructor
 		
 	}
 
-	public void error() {
-		try{
-			Scanner keyboard = new Scanner(System.in);
+	public void error() { //error() method detects an input error
+		try{ //try block to detect exceptions
+			Scanner keyboard = new Scanner(System.in); //instantiate
 
-			System.out.print("x: ");
-			x = keyboard.nextInt();
-			System.out.print("y: ");
-			y = keyboard.nextInt();
+			System.out.print("x: "); //print the line
+			x = keyboard.nextInt(); //get the value
+			System.out.print("y: "); //print the line
+			y = keyboard.nextInt(); //get the value
 			
-			System.out.println(x/y);
+			System.out.println(x/y); //print the value
 
 		}
-		catch(InputMismatchException e) {
-			e = new InputMismatchException("java.util.InputMismatchException");
-			System.out.println(e.getMessage());
+		catch(InputMismatchException e) { //when Input is not integer, the predefined exception is thrown, and this catch block catches it.
+			e = new InputMismatchException("java.util.InputMismatchException"); //create new e instance with the String parameter
+			System.out.println(e.getMessage()); //print the String returned by getMessage()
 			
-//			System.out.println("java.util.InputMismatchException");
+			//other way
+			//System.out.println("java.util.InputMismatchException");
 		}
-		catch(ArithmeticException e) {
-			e = new ArithmeticException("java.lang.ArithmeticException: / by zero");
-			System.out.println(e.getMessage());
+		catch(ArithmeticException e) { //catch block for thrown ArithmeticException instance
+			e = new ArithmeticException("java.lang.ArithmeticException: / by zero"); //create new e instance with the paramter
+			System.out.println(e.getMessage()); //print the String which is the constructor parameter
 			
-//			System.out.print("java.lang.ArithmeticException: ");
-//			System.out.println(e.getMessage());
+			//other way
+			//System.out.print("java.lang.ArithmeticException: "); //print the line
+			//System.out.println(e.getMessage()); //print "/ by zero " which is a default message value
 		}
 		
 		
