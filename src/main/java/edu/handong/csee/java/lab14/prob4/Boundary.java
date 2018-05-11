608c6f20f12919f483630992df1b0c5be43644ba
JoJeonghoon <akima95@naver.com>
2018-05-11 11:29:19 +0900

    Add single line comments for Boundary class

--- src/main/java/edu/handong/csee/java/lab14/prob4/Boundary.java
+++ src/main/java/edu/handong/csee/java/lab14/prob4/Boundary.java
@@ -1,21 +1,21 @@
-package edu.handong.csee.java.lab14.prob4;
+package edu.handong.csee.java.lab14.prob4; //package name
 
-public class Boundary {
+public class Boundary { //Boundary class
 
-	private static int[] arr = new int[5];
-	private static int i =0;
+	private static int[] arr = new int[5]; //array with size 5
+	private static int i =0; //static variable
 
-	public Boundary() {
+	public Boundary() { //constructor
 
 	}
 
-	static public void receiveInput(int a) {
+	static public void receiveInput(int a) { //this static method receives inputs by a parameter
 
-		arr[i] = a;
+		arr[i] = a; //set array element value by input
 
-		System.out.println("arr["+ i + "]<- " + arr[i]);
+		System.out.println("arr["+ i + "]<- " + arr[i]); //print array element value
 
-		i++;
+		i++; //increase one for a next array element
 
 	}
 
