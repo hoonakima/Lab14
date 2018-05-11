a512119c4d5affb34deb307ea8715229a40d42ea
JoJeonghoon <akima95@naver.com>
2018-05-11 13:01:57 +0900

    Add a MyException class

---
+++ src/main/java/edu/handong/csee/java/lab14/prob5/MyException.java
@@ -0,0 +1,20 @@
+package edu.handong.csee.java.lab14.prob5;
+
+public class MyException extends Exception {
+
+	String message = null; 
+	
+	public MyException() {
+		super();
+	}
+	
+	public MyException(String message) {
+		super(message);
+		this.message = message;
+	} 
+	
+	public String toString() {
+		return message;
+	}
+
+}
