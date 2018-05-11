54461232e48d9b9472ef58e98234421972f820c3
JoJeonghoon <akima95@naver.com>
2018-05-11 11:34:17 +0900

    Add single line comments for Main class

--- src/main/java/edu/handong/csee/java/lab14/prob4/Main.java
+++ src/main/java/edu/handong/csee/java/lab14/prob4/Main.java
@@ -1,27 +1,27 @@
-package edu.handong.csee.java.lab14.prob4;
+package edu.handong.csee.java.lab14.prob4; //package name
 
-import java.util.Scanner;
+import java.util.Scanner; //import Scanner class
 
-public class Main {
+public class Main { //Main class
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { //starts this program
 		
-		boolean repeat = true;
+		boolean repeat = true; //it makes to start the while loop
 
-		try {
-			Scanner in = new Scanner(System.in);
+		try { //try block to throw exceptions
+			Scanner in = new Scanner(System.in); //instantiate
 
-			while(repeat){
+			while(repeat){ //iterates the loop while repeat is true
 
-				System.out.print("Enter an integer: ");
-				int a =in.nextInt();
-				Boundary.receiveInput(a);	
+				System.out.print("Enter an integer: "); //print the line
+				int a =in.nextInt(); //get input value
+				Boundary.receiveInput(a); //call the method to send an input value
 
 			}
 		}
-		catch(ArrayIndexOutOfBoundsException e) {
-			System.out.println("Invalid array index access!");
-			repeat = false;
+		catch(ArrayIndexOutOfBoundsException e) {  //catch a created exception 
+			System.out.println("Invalid array index access!"); //print the error message
+			repeat = false; //stop the while loop
 		}
 	}
 
