7a9aa5a9f55ac271c770df181b6fa71a7cb19f1b
JoJeonghoon <akima95@naver.com>
2018-05-11 13:05:33 +0900

    Add single line comments for MyException class

--- src/main/java/edu/handong/csee/java/lab14/prob5/MyException.java
+++ src/main/java/edu/handong/csee/java/lab14/prob5/MyException.java
@@ -1,20 +1,20 @@
-package edu.handong.csee.java.lab14.prob5;
+package edu.handong.csee.java.lab14.prob5; //package name
 
-public class MyException extends Exception {
+public class MyException extends Exception { //customized exception class that inherits Exception class
 
-	String message = null; 
+	String message = null;  //message data
 	
-	public MyException() {
-		super();
+	public MyException() { //default constructor
+		super(); //call super constructor
 	}
 	
-	public MyException(String message) {
-		super(message);
-		this.message = message;
+	public MyException(String message) { //constructor
+		super(message); //call super constructor
+		this.message = message; //set message data as input parameter
 	} 
 	
-	public String toString() {
-		return message;
+	public String toString() { //overrides toString() method
+		return message; //return message data
 	}
 
 }
