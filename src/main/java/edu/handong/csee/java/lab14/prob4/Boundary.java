f86b47d43082164e4d3dd857d68d65fdd3109352
JoJeonghoon <akima95@naver.com>
2018-05-11 11:25:15 +0900

    Add a real Boundary class

--- src/main/java/edu/handong/csee/java/lab14/prob4/Boundary.java
+++ src/main/java/edu/handong/csee/java/lab14/prob4/Boundary.java
@@ -2,4 +2,21 @@ package edu.handong.csee.java.lab14.prob4;
 
 public class Boundary {
 
+	private static int[] arr = new int[5];
+	private static int i =0;
+
+	public Boundary() {
+
+	}
+
+	static public void receiveInput(int a) {
+
+		arr[i] = a;
+
+		System.out.println("arr["+ i + "]<- " + arr[i]);
+
+		i++;
+
+	}
+
 }
